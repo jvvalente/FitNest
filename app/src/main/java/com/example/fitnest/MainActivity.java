88b852ab732adeea12b537948aba1d4bdedbcad8
jvvalente <joaovalente2010@icloud.com>
2021-03-02 21:09:30 -0500

    bottom navigation

--- app/src/main/java/com/example/fitnest/MainActivity.java
+++ app/src/main/java/com/example/fitnest/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.fitnest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment2);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
+
 }
