d7dc10dafc57d7acfedfa916f464ddaeb9e67edd
Cathy Yue <cy18@my.fsu.edu>
2021-03-23 15:51:53 -0400

    Fixed bugs. Got expandable recyclerView on the main workout page done, and also added in a number picker feature.

--- app/src/main/java/com/example/fitnest/WorkoutItem.java
+++ app/src/main/java/com/example/fitnest/WorkoutItem.java
@@ -1,15 +1,33 @@
 package com.example.fitnest;
 
+import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class WorkoutItem {
-    YouTubePlayerView youTubePlayerView;
+public class WorkoutItem  {
 
+    //Workout Item functions
+    YouTubePlayerView youTubePlayerView;
     private String workoutType;
+    private boolean expanded;
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
 
     public WorkoutItem(String workoutType) {
         this.workoutType = workoutType;
+        this.expanded = false;
     }
 
     public String getWorkoutType() {
@@ -24,4 +42,7 @@ public class WorkoutItem {
         this.workoutType = workoutType;
     }
 
+    public void setNumberPicker(){
+
+    }
 }
--- app/src/main/java/com/example/fitnest/adapters/WorkoutAdapter.java
+++ app/src/main/java/com/example/fitnest/adapters/WorkoutAdapter.java
@@ -1,21 +1,28 @@
 package com.example.fitnest.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fitnest.R;
 import com.example.fitnest.WorkoutItem;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 import java.util.ArrayList;
 
 public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHolder>{
-
+    public static final String YOUTUBE_API_KEY = "AIzaSyAIF6220mZ2d1v53FU7cvOeSh2as1kmT44";
     Context context;
     private ArrayList<WorkoutItem> workoutList;
 
@@ -34,6 +41,9 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHold
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String workout = workoutList.get(position).getWorkoutType();
         holder.tvWorkoutType.setText(workout);
+
+        boolean isExpanded = workoutList.get(position).isExpanded();
+        holder.expandableLayout.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
     }
 
     @Override
@@ -45,11 +55,60 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHold
 
         TextView tvWorkoutType;
         TextView tvWorkoutInfo;
+        NumberPicker numberPicker;
+        YouTubePlayerView youTubePlayerView;
+        ConstraintLayout workoutItem;
+        ConstraintLayout expandableLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvWorkoutType = itemView.findViewById(R.id.tvWorkoutType);
+            setNumberPicker(itemView);
+            //initializeYouTube(itemView);
+            workoutItem = itemView.findViewById(R.id.workoutItem);
+            expandableLayout = itemView.findViewById(R.id.expandableLayout);
+
+            workoutItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    WorkoutItem workout = workoutList.get(getAdapterPosition());
+                    workout.setExpanded(!workout.isExpanded());
+                    notifyItemChanged(getAdapterPosition());
+                }
+            });
         }
+
+        //sets the number picker
+        private void setNumberPicker(View itemView) {
+            numberPicker = itemView.findViewById(R.id.numberPicker);
+            numberPicker.setMinValue(0);
+            numberPicker.setMaxValue(10);
+            numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+                @Override
+                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                }
+            });
+        }
+
+        /*
+        //initalizes the YoutTubePlayerView
+        private void initializeYouTube(View itemView){
+            youTubePlayerView = itemView.findViewById(R.id.youtubePlayerView);
+            youTubePlayerView.initialize(YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                    Log.d("WorkoutFragment", "onInitializationSuccess");
+                    youTubePlayer.cueVideo("tKodtNFpzBA");
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                    Log.d("WorkoutFragment", "onInitializationFailure");
+                }
+            });
+        }
+
+         */
     }
 }
 
