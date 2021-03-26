b2dd7ade286f207b09642be45c8c4565c7055592
jvvalente <joaovalente2010@icloud.com>
2021-03-26 17:11:10 -0400

    Connect most pages

---
+++ app/src/main/java/com/example/fitnest/ItemPersonalPlans.java
@@ -0,0 +1,19 @@
+package com.example.fitnest;
+
+public class ItemPersonalPlans {
+
+    private String workoutName;
+
+    public ItemPersonalPlans(String workoutName) {
+        this.workoutName = workoutName;
+    }
+
+    public String getWorkoutName() {
+        return workoutName;
+    }
+
+    public void setWorkoutName(String workoutName) {
+        this.workoutName = workoutName;
+    }
+
+}
--- app/src/main/java/com/example/fitnest/WorkoutFragment.java
+++ app/src/main/java/com/example/fitnest/WorkoutFragment.java
@@ -1,9 +1,12 @@
 package com.example.fitnest;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
@@ -115,5 +119,16 @@ public class WorkoutFragment extends Fragment {
         });
 
         return v;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_button:
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
