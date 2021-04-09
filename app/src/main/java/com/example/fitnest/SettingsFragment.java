264e415a99cc07478190424c55656c518bb2c677
Herno <Hernolas08@gmail.com>
2021-04-09 11:22:57 -0400

    settings now an activity

--- app/src/main/java/com/example/fitnest/SettingsFragment.java
+++ app/src/main/java/com/example/fitnest/SettingsFragment.java
@@ -1,14 +1,8 @@
 package com.example.fitnest;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
