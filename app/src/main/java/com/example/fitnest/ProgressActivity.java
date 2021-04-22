dc0fbb721effcf0aabac7721b3d3e84ca60c31d3
ap16at <ap16at@my.fsu.edu>
2021-04-21 20:06:21 -0400

    changes to adapter

--- app/src/main/java/com/example/fitnest/ProgressActivity.java
+++ app/src/main/java/com/example/fitnest/ProgressActivity.java
@@ -1,96 +1,96 @@
-package com.example.fitnest;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.Toast;
-
-import com.example.fitnest.adapters.GoalsToAccomplishAdapter;
-import com.example.fitnest.adapters.PersonalPlansAdapter;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.SaveCallback;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-public class ProgressActivity extends AppCompatActivity implements PersonalPlansAdapter.ItemClickListener{
-
-    EditText newWorkoutGroup;
-
-    GoalsToAccomplishAdapter adapter;
-
-    ArrayList<String> goals;
-
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-
-    Context context;
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_progress);
-
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
-        query.selectKeys(Arrays.asList("Goal"));
-        query.addDescendingOrder("Goal");
-
-        goals = new ArrayList<>();
-
-        context = getBaseContext();
-
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if (e == null) {
-
-                    for (ParseObject o: objects) {
-                        System.out.println("ParseObject: " + o.get("Goal"));
-                        goals.add(o.get("Goal").toString());
-                    }
-
-                    setUpAdapter(goals);
-
-                    Toast.makeText(ProgressActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Something is wrong
-                    Toast.makeText(ProgressActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    public void setUpAdapter(ArrayList<String> goals){
-        // set up the RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.rvToAccomplish);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new GoalsToAccomplishAdapter(this, goals);
-        recyclerView.setAdapter(adapter);
-    }
-
-    @Override
-    public void onItemClick(View view, int position) {
-
-    }
-}
+//package com.example.fitnest;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import android.app.AlertDialog;
+//import android.content.Context;
+//import android.content.DialogInterface;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.Gravity;
+//import android.view.LayoutInflater;
+//import android.view.MotionEvent;
+//import android.view.View;
+//import android.widget.EditText;
+//import android.widget.LinearLayout;
+//import android.widget.PopupWindow;
+//import android.widget.Toast;
+//
+//import com.example.fitnest.adapters.GoalsToAccomplishAdapter;
+//import com.example.fitnest.adapters.PersonalPlansAdapter;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//import com.parse.FindCallback;
+//import com.parse.ParseException;
+//import com.parse.ParseObject;
+//import com.parse.ParseQuery;
+//import com.parse.SaveCallback;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class ProgressActivity extends AppCompatActivity implements PersonalPlansAdapter.ItemClickListener{
+//
+//    EditText newWorkoutGroup;
+//
+//    GoalsToAccomplishAdapter adapter;
+//
+//    ArrayList<String> goals;
+//
+//    private RecyclerView mRecyclerView;
+//    private RecyclerView.Adapter mAdapter;
+//    private RecyclerView.LayoutManager mLayoutManager;
+//
+//    Context context;
+//
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.fragment_progress);
+//
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+//        query.selectKeys(Arrays.asList("Goal"));
+//        query.addDescendingOrder("Goal");
+//
+//        goals = new ArrayList<>();
+//
+//        context = getBaseContext();
+//
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            @Override
+//            public void done(List<ParseObject> objects, ParseException e) {
+//                if (e == null) {
+//
+//                    for (ParseObject o: objects) {
+//                        System.out.println("ParseObject: " + o.get("Goal"));
+//                        goals.add(o.get("Goal").toString());
+//                    }
+//
+//                    setUpAdapter(goals);
+//
+//                    Toast.makeText(ProgressActivity.this, "Success", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    // Something is wrong
+//                    Toast.makeText(ProgressActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
+//
+//    public void setUpAdapter(ArrayList<String> goals){
+//        // set up the RecyclerView
+//        RecyclerView recyclerView = findViewById(R.id.rvToAccomplish);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        adapter = new GoalsToAccomplishAdapter(this, goals);
+//        recyclerView.setAdapter(adapter);
+//    }
+//
+//    @Override
+//    public void onItemClick(View view, int position) {
+//
+//    }
+//}
--- app/src/main/java/com/example/fitnest/adapters/GoalsToAccomplishAdapter.java
+++ app/src/main/java/com/example/fitnest/adapters/GoalsToAccomplishAdapter.java
@@ -1,69 +1,112 @@
 package com.example.fitnest.adapters;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fitnest.ToAccomplishItem;
 import com.example.fitnest.R;
+import com.example.fitnest.WorkoutItem;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class GoalsToAccomplishAdapter extends RecyclerView.Adapter<GoalsToAccomplishAdapter.ViewHolder>{
 
-    private List<String> mData;
+    private List<String> goals;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
 
+    // NEW CODE
+    Context context;
+    ArrayList<ToAccomplishItem> goalList;
+
     public GoalsToAccomplishAdapter(Context context, List<String> data){
         this.mInflater = LayoutInflater.from(context);
-        this.mData = data;
+        this.goals = data;
+        this.context = context;
+        this.goalList = goalList;
     }
 
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.goals_to_accomplish, parent, false);
+//        View view = mInflater.inflate(R.layout.goals_to_accomplish, parent, false);
+
+        Log.d("GoalsToAccomplishAdapter", "onCreateViewHolder");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.goals_to_accomplish, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        String animal = mData.get(position);
-        holder.myTextView.setText(animal);
+//        String animal = mData.get(position);
+//        holder.myTextView.setText(animal);
     }
 
     @Override
     public int getItemCount() {
-        return mData.size();
+        return goals.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView myTextView;
+        ConstraintLayout goalItem;
 
-        ViewHolder(View itemView) {
+        ViewHolder(@NonNull View itemView) {
             super(itemView);
             myTextView = itemView.findViewById(R.id.tvAccomplishmentTo);
+
+            ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
+            query.selectKeys(Arrays.asList("Goal"));
+            query.addDescendingOrder("Goal");
+
+            goals = new ArrayList<>();
+
+            query.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if (e == null) {
+
+                        for (ParseObject o: objects) {
+                            System.out.println("ParseObject: " + o.get("Goal"));
+                            goals.add(o.get("Goal").toString());
+                        }
+
+//                        Toast.makeText(ProgressActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Something is wrong
+//                        Toast.makeText(ProgressActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-            System.out.println("This works!" + getAdapterPosition() + " " + getItem(getAdapterPosition()));
 
-
+            notifyItemChanged(getAdapterPosition());
         }
     }
 
     String getItem(int id) {
-        return mData.get(id);
+        return goals.get(id);
     }
 
     // allows clicks events to be caught
